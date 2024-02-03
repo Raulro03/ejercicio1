40e5cf0ade9fe3f6fcd122c2e4705f6c7cb3c5b7
Raulro03 <raulromeraortiz@gmail.com>
2024-02-03 18:45:47 +0100

    Añadir metodo fechayhora en Principal

--- Principal.java
+++ Principal.java
@@ -1,6 +1,13 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 public class Principal {
     public static void main (String [] args) {
         System.out.println("Hola mundo");
+        horayfecha();
        
     }
+    public static void horayfecha () {
+        String dateTime = DateTimeFormatter.ofPattern("dd MM yyyy, hh:mm:ss a").format(LocalDateTime.now());
+        System.out.println(dateTime); 
+    }   
 }
--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 Practica 4.1 de Git en entornos
 
 ## Ejemplo01.java
-> Este fichero es un ejemplo de un hola mundo y crea una variable con un nombre que luego muestra un mensaje
+> Este fichero es un ejemplo de un hola mundo y muestra la fecha y hora actuales
+> Que puedo decir sobre el metodo fecha y hora, es un metodo que gracias a la libreria LocalDateTime podemos con el comando DateTimeFormatter.ofPattern hacerlo
